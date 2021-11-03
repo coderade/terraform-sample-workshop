835cb853e4fee8e660965221b55a4a99efb6f46c
Valdeci Gomes <valdeci@outlook.my>
2021-11-03 10:43:44 -0300

    Update generate_ssh_key.sh

--- module_1/one_file_tf/simple_nginx_stack/scripts/generate_ssh_key.sh
+++ module_1/one_file_tf/simple_nginx_stack/scripts/generate_ssh_key.sh
@@ -8,7 +8,7 @@ echo "Press enter 3 times"
 
 ssh-keygen
 
-aws ec2 import-key-pair --key-name "nginx-demo-key" --public-key-material file://./id_rsa.pub
+aws ec2 import-key-pair --key-name "nginx-demo-key" --public-key-material file:///home/ec2-user/.ssh/id_rsa.pub
 
 echo "Key imported!"
 
